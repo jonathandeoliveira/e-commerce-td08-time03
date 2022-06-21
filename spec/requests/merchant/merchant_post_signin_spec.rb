a892dec7f7ac5e65498d4ee77a0b0e34962be89d
Jonathan Gonçalves <jonathan_oliveira1997@hotmail.com>
2022-06-21 20:17:18 -0300

    bugfix: refatoração de código de views, criação de testes complementares para models existentes
    Co-Authored-By: lucascda <43433070+lucascda@users.noreply.github.com>

---
+++ spec/requests/merchant/merchant_post_signin_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'mercador faz request de login' do
+  it 'com sucesso' do
+    customer = create(:merchant)
+
+    post merchant_session_path( params: {merchant: {email: customer.email, password: customer.password }})
+
+    expect(response).to redirect_to root_path
+
+  end
+
+  it 'sem sucesso, pois campos em branco' do
+    post merchant_session_path( params: {merchant: {email: '', password: '' }})
+
+    expect(response).to render_template('devise/sessions/new')
+  end
+end
