f811897268f95895ed4e7b4fa8bb8aac8d9b8cc0
fmarga <marga.fmartins@gmail.com>
2022-07-01 20:33:38 -0300

    fix: refatora testes de model para order e refatora códigos com identação e espaçamento errados
    Co-Authored-By: Raifc <raif.si@hotmail.com>

--- app/models/product_price.rb
+++ app/models/product_price.rb
@@ -8,31 +8,31 @@ class ProductPrice < ApplicationRecord
 
   private 
   def date_is_future
-    if self.start_date.present? && self.start_date < Date.today
+    if start_date.present? && start_date < Date.today
       self.errors.add(:start_date,' não pode ser no passado.')
     end
-    if self.end_date.present? && self.end_date < Date.today
+    if end_date.present? && end_date < Date.today
       self.errors.add(:end_date, ' não pode ser no passado.')
     end
   end
 
   def start_date_differs_from_end_date
-    if self.start_date.present? && self.end_date.present? && self.start_date == self.end_date
+    if start_date.present? && end_date.present? && start_date == end_date
       self.errors.add(:start_date, ' e data final não podem ser iguais.')
     end
   end
 
   def end_date_is_future_from_start_date
-    if self.end_date.present? && self.start_date.present? && self.end_date < self.start_date
+    if end_date.present? && start_date.present? && end_date < start_date
       self.errors.add(:end_date, ' não pode ser anterior à data inicial.')
     end
   end
 
   def price_unique_within_dates
-    if self.price.present? && self.start_date.present? && self.end_date.present?
+    if price.present? && start_date.present? && end_date.present?
       prices = ProductPrice.where(product_model: self.product_model_id) - [self]
       prices.each do | price |
-        if (self.start_date >= price.start_date && self.start_date <= price.end_date) || (self.end_date >= price.start_date && self.end_date <= price.end_date)
+        if (start_date >= price.start_date && start_date <= price.end_date) || (end_date >= price.start_date && end_date <= price.end_date)
           self.errors.add(:price, ' não pode ser cadastrado, pois está incluso em intervalo de datas já existentes')
         end
       end
