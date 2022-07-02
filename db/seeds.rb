9311644fda8268c230f773f4d2931c63015f275a
Raif <raif.si@hotmail.com>
2022-07-02 17:31:00 -0300

    refactor: adiciona imagem aos seeds e faz correções na homepage
    Co-Authored-By: jonathandeoliveira <jonathan_oliveira1997@hotmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,7 @@ informatica = Category.create!(name: 'Informática', status: 0)
 games = Category.create!(name: 'Games', status: 0)
 livros = Category.create!(name: 'Livros', status: 0)
 eletrodomesticos = Category.create!(name: 'Eletrodomésticos', status: 0)
+veiculos = Category.create!(name: 'Veículos', status: 0)
 
 # sub-categories
 computadores = SubCategory.create!(name: 'Computadores', category: eletronicos)
@@ -34,22 +35,66 @@ hqs = SubCategory.create!(name: 'HQs e Mangás', category: livros)
 lavadoras = SubCategory.create!(name: 'Lavadoras e Secadoras', category: eletrodomesticos)
 geladeiras = SubCategory.create!(name: 'Geladeiras e Freezers', category: eletrodomesticos)
 fogoes = SubCategory.create!(name: 'Fogões e Fornos', category: eletrodomesticos)
-
+teclados = SubCategory.create!(name: 'Teclados', category: eletronicos)
+eletricos = SubCategory.create!(name: 'Elétricos', category: veiculos)
 # products
+
+ideapad = ProductModel.create!(name: 'Notebook Lenovo IdeaPad', brand: 'Lenovo', sku: 'LENOVOIDEA', model: 'IdeaPad 3i i3', description: 'Notebook 15.6 Memória 8GB CPU: Core i3-10110U', weight: 2, height: 2, width: 36, length: 25, sub_category: notebooks, status: 0)
+ideapad.photos.attach(io: File.open(Rails.root.join('app/assets/images/notebook1.jpg')), filename: 'notebook1.jpg')
+ideapad.photos.attach(io: File.open(Rails.root.join('app/assets/images/notebook2.jpg')), filename: 'notebook2.jpg')
+ideapad.photos.attach(io: File.open(Rails.root.join('app/assets/images/notebook3.jpg')), filename: 'notebook3.jpg')
+
+
+electrolux_jet_clean = ProductModel.create!(name: 'Máquina de Lavar Electrolux', brand: 'Electrolux', sku: 'JETCLEAN13CARE', model: 'Jet & Clean', description: 'Máquina de Lavar 13kg Essential Care 220V', weight: 30, height: 105.2, width: 59.1, length: 66.5, sub_category: lavadoras, status: 0)
+electrolux_jet_clean.photos.attach(io: File.open(Rails.root.join('app/assets/images/Eletrolux1.png')), filename: 'Eletrolux1.png')
+electrolux_jet_clean.photos.attach(io: File.open(Rails.root.join('app/assets/images/Eletrolux2.jpg')), filename: 'Eletrolux2.jpg')
+
 iphone = ProductModel.create!(name: 'iPhone 11', brand: 'Apple', sku: 'IPHONE11-128GB', model: 'iPhone 11 Branco', description: 'iPhone 128GB desbloqueado', weight: 0.2, height: 15, width: 7.5, length: 0.8, sub_category: celulares, status: 0)
-ideapad = ProductModel.create!(name: 'Notebook Lenovo Ultrafino IdeaPad', brand: 'Lenovo', sku: 'LENOVOIDEA', model: 'IdeaPad 3i i3', description: 'Notebook 15.6 Memória 8GB CPU: Core i3-10110U', weight: 2, height: 2, width: 36, length: 25, sub_category: notebooks, status: 0)
-electrolux_jet_clean = ProductModel.create!(name: 'Máquina de Lavar Electrolux Essential Care', brand: 'Electrolux', sku: 'JETCLEAN13CARE', model: 'Jet & Clean', description: 'Máquina de Lavar 13kg Essential Care com cesto inox ultra filter 220V', weight: 30, height: 105.2, width: 59.1, length: 66.5, sub_category: lavadoras, status: 0)
+iphone.photos.attach(io: File.open(Rails.root.join('app/assets/images/iphone1.jpg')), filename: 'iphone1.jpg')
+iphone.photos.attach(io: File.open(Rails.root.join('app/assets/images/iphone2.jpg')), filename: 'iphone2.jpg')
+iphone.photos.attach(io: File.open(Rails.root.join('app/assets/images/iphone3.jpg')), filename: 'iphone3.jpg')
+
+teclado_hyperx = ProductModel.create!(name: 'Teclado Alloy', brand: 'Hyperx', sku: 'HYPERX2022', model: 'Alloy', description: 'Teclado mecanico',weight: 0.7, height: 8, width: 30, length: 1, sub_category: teclados, status: 0)
+teclado_hyperx.photos.attach(io: File.open(Rails.root.join('app/assets/images/teclado1.jpg')), filename: 'teclado1.jpg')
+teclado_hyperx.photos.attach(io: File.open(Rails.root.join('app/assets/images/teclado-2.jpg')), filename: 'teclado-2.jpg')
+teclado_hyperx.photos.attach(io: File.open(Rails.root.join('app/assets/images/teclado-3.jpg')), filename: 'teclado-3.jpg')
+
+
+polystation = ProductModel.create!(name: 'Polystation 5',brand: 'Poly',sku: 'P0LYST4T10N', model: 'Poly Machine 5',fragile: true,description: 'O melhor console da atualidade', weight: 4.0,height: 0.3,width: 0.2, length: 0.3,sub_category: playstation, status: 0)
+polystation.photos.attach(io: File.open(Rails.root.join('app/assets/images/poly1.jpg')), filename: 'poly1.jpg')
+polystation.photos.attach(io: File.open(Rails.root.join('app/assets/images/poly2.jpg')), filename: 'poly2.jpg')
+polystation.photos.attach(io: File.open(Rails.root.join('app/assets/images/poly3.jpg')), filename: 'poly3.jpg')
+
+batmobile = ProductModel.create!(name: 'Batmovel', brand: 'batcompany', sku: 'B4TM0B1L3', model: 'eletrico', fragile: false, description: 'Carro morcego', weight: 800.0, height: 2.0, width: 2.0, length: 2.0, sub_category: eletricos, status:0)
+batmobile.photos.attach(io: File.open(Rails.root.join('app/assets/images/batmovel.jpg')), filename: 'batmovel.jpg')
+batmobile.photos.attach(io: File.open(Rails.root.join('app/assets/images/batmovel.jpg')), filename: 'batmovel2.jpg')
+
+blackshark = ProductModel.create!(name: 'Black Shark 4', brand: 'Xiaomi', sku: 'BL4CK-SH4RK', model: 'Black Shark 4', fragile: true, description: 'Celular Gamer', weight: 0.1, height: 0.1, width: 0.1, length: 0.1, sub_category: celulares, status:0)
+blackshark.photos.attach(io: File.open(Rails.root.join('app/assets/images/blackshark1.png')), filename: 'blackshark1.jpg')
+blackshark.photos.attach(io: File.open(Rails.root.join('app/assets/images/blackshark1.png')), filename: 'blackshark2.jpg')
+blackshark.photos.attach(io: File.open(Rails.root.join('app/assets/images/blackshark1.png')), filename: 'blackshark3.jpg')
+
 
 # product-prices
 iphone_preco1 = ProductPrice.create!(price: 4000.5, start_date: Date.today, end_date: 30.day.from_now, product_model: iphone)
 iphone_preco2 = ProductPrice.create!(price: 3700.5, start_date: 31.day.from_now, end_date: 61.day.from_now, product_model: iphone)
 iphone_preco3 = ProductPrice.create!(price: 3950.0, start_date: 62.day.from_now, end_date: 92.day.from_now, product_model: iphone)
+
 idea_preco1 = ProductPrice.create!(price: 2800.0, start_date: Date.today, end_date: 30.day.from_now, product_model: ideapad)
 idea_preco2 = ProductPrice.create!(price: 2900.0, start_date: 31.day.from_now, end_date: 61.day.from_now, product_model: ideapad)
 idea_preco3 = ProductPrice.create!(price: 3400.8, start_date: 62.day.from_now, end_date: 92.day.from_now, product_model: ideapad)
+
 jet_clean_preco1 = ProductPrice.create!(price: 2000.3, start_date: Date.today, end_date: 30.day.from_now, product_model: electrolux_jet_clean)
 jet_clean_preco2 = ProductPrice.create!(price: 1760.5, start_date: 31.day.from_now, end_date: 61.day.from_now, product_model: electrolux_jet_clean)
 jet_clean_preco3 = ProductPrice.create!(price: 1950.5, start_date: 62.day.from_now, end_date: 92.day.from_now, product_model: electrolux_jet_clean)
+
+teclado_hyperx_preco1 = ProductPrice.create!(price: 500, start_date: Date.today, end_date: 90.day.from_now, product_model: teclado_hyperx)
+
+polystation_preco1 = ProductPrice.create!(price: 2999.99, start_date: Date.today, end_date: 90.day.from_now, product_model: polystation)
+
+batmobile_preco1 = ProductPrice.create!(price: 4459999.99, start_date: Date.today, end_date: 90.day.from_now, product_model: batmobile)
+
+blackshark_preco1 = ProductPrice.create!(price: 2999.99, start_date: Date.today, end_date: 90.day.from_now, product_model: blackshark)
 
 # product items (shopping cart)
 first_product_item = ProductItem.create!(customer: joao, product_model: iphone, quantity: 2)
@@ -57,5 +102,5 @@ second_product_item = ProductItem.create!(customer: joao, product_model: ideapad
 third_product_item = ProductItem.create!(customer: erika, product_model: ideapad, quantity: 5)
 
 # promotions
-first_promotion = Promotion.create!(name: 'CAMPUSDESCONTO', start_date: Date.today, end_date: 30.day.from_now, max_quantity: 20, used_quantity: 0, discount_percent: 5, max_discount_money: 0.4, sub_categories: [computadores, celulares])
-second_promotion = Promotion.create!(name: 'SUPERDESCONTO', start_date: 30.day.from_now, end_date: 60.day.from_now, max_quantity: 20, used_quantity: 0, discount_percent: 15, max_discount_money: 0.4, sub_categories: [lavadoras, geladeiras, smartwatches, computadores])
+#first_promotion = Promotion.create!(name: 'CAMPUSDESCONTO', start_date: Date.today, end_date: 30.day.from_now, max_quantity: 20, used_quantity: 0, discount_percent: 5, max_discount_money: 0.4, sub_categories: [computadores, celulares])
+#second_promotion = Promotion.create!(name: 'SUPERDESCONTO', start_date: 30.day.from_now, end_date: 60.day.from_now, max_quantity: 20, used_quantity: 0, discount_percent: 15, max_discount_money: 0.4, sub_categories: [lavadoras, geladeiras, smartwatches, computadores])
