26bc1ecec4d3df9c8c87eb7717ee6b5c93654982
fmarga <marga.fmartins@gmail.com>
2022-07-02 16:47:40 -0300

    refatora teste que não estava passando

--- spec/system/customer/customer_buys_credit_spec.rb
+++ spec/system/customer/customer_buys_credit_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Usuário compra crédito de rubis' do
-  it 'a partir da página de detalhes da conta com sucesso' do
+  it 'a partir da página de detalhes da conta sem sucesso, pois não possui conexão com a aplicação de pagamentos' do
     customer = create(:customer)
 
     login_as(customer, scope: :customer)
@@ -10,9 +10,8 @@ describe 'Usuário compra crédito de rubis' do
     click_on 'Comprar crédito'
     fill_in 'Valor em reais', with: 1000
     click_on 'Comprar'
-    # response = double('faraday_response', body: json, status: 200)
-    # Allow(Faraday).to receive(:post).and_return(response)
+
     customer.reload
-    expect(page).to have_content 'Solicitação de créditos realizada com sucesso'
+    expect(page).to have_content 'Falha na solicitação, tente novamente mais tarde'
   end
 end
